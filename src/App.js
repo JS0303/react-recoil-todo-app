833a5848cc93b9cd29f868909084a7754760dd0c
js0303 <wlstjr0303@gmail.com>
2023-03-09 14:57:17 +0900

    Todo List 수정 기능 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useRecoilValue } from "recoil";
 import "./App.css";
+import TodoItem from "./components/TodoItem";
 import TodoItemCreator from "./components/TodoItemCreator";
+import { todoListState } from "./todoAtom";
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
   return (
     <div className="App">
       <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </div>
   );
 }
