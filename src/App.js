ac122bee3ce85b88aa3d5d6737fbc04ac9af6b75
js0303 <wlstjr0303@gmail.com>
2023-03-09 15:53:30 +0900

    비동기 데이터 쿼리

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Suspense } from "react";
 import { useRecoilValue } from "recoil";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
@@ -5,12 +6,16 @@ import TodoItemCreator from "./components/TodoItemCreator";
 import TodoListFilters from "./components/TodoListFilters";
 import TodoListStats from "./components/TodoListStats";
 import { filteredTodoListState, todoListState } from "./todoAtoms";
+import { currentUserNameQuery } from "./userAtoms";
 
 function App() {
-  const todoList = useRecoilValue(todoListState);
+  // const todoList = useRecoilValue(todoListState);
   const filteredTodoList = useRecoilValue(filteredTodoListState);
   return (
     <div className="App">
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <CurrentUserInfo />
+      </React.Suspense>
       <TodoListStats />
       <TodoListFilters />
       <TodoItemCreator />
@@ -22,3 +27,8 @@ function App() {
 }
 
 export default App;
+
+function CurrentUserInfo() {
+  const userName = useRecoilValue(currentUserNameQuery);
+  return <div>{userName}</div>;
+}
